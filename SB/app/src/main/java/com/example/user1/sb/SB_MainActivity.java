df9130b5f2f0136e67e0651c9be0e701ae2c9ae2
U10316007 <theanswer0411@gmail.com>
2016-12-18 13:34:35 +0800

    SB_version1
    ver1

---
+++ SB/app/src/main/java/com/example/user1/sb/SB_MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.user1.sb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SB_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sb__main);
+    }
+}
